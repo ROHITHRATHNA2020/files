abc327a61a212a719cd0c68c94fb504b1c0bc09e
ROHITHRATHNA2020 <rohithrathna916@gmail.com>
2021-08-26 18:44:47 +0530

    files slider

---
+++ app/src/main/java/com/example/files/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.files;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.example.files.fragments.HelpFragment;
+import com.example.files.fragments.SettingFragment;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+
+    }
+
+    private void initViews() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.Open_Drawer, R.string.Close_Drawer);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.settings:
+                SettingFragment settingFragment = new SettingFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,settingFragment).addToBackStack(null).commit();
+                break;
+            case R.id.help:
+                HelpFragment helpFragment = new HelpFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,helpFragment).addToBackStack(null).commit();
+                break;
+            case R.id.privacy:
+                Toast.makeText(this,"privacy",Toast.LENGTH_SHORT).show();
+                break;
+        }
+    drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        getSupportFragmentManager().popBackStackImmediate();
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+
+    }
+}
